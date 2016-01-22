7aff280ebafe22f69ed8ddb6b1a9f25429147b4e
Syver Enstad <syver.enstad@gmail.com>
2016-01-22 02:10:51 +0100

    Add edit to the notes app
     - experiment with not using jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import uuid from 'node-uuid';
-//import Note from './Note.jsx';
+import Note from './Note';
 
-var App = React.createClass({
-  getInitialState: function() {
-    return {
+
+var Notes = ({notes, onEdit}) => {
+  return (
+      <ul>{notes.map((note) =>
+                     <li key={note.id}>
+                     <Note task={note.task} onEdit={onEdit.bind(null, note.id)}/>
+                     </li>
+                    )}
+    </ul>);
+}
+
+
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       notes: [
         {
           id: uuid.v4(),
@@ -20,26 +34,31 @@ var App = React.createClass({
         }
       ]
     }
-  },
-  render: function() {
+  }
+  render() {
     return (
         <div>
         <button onClick={this.addNote}>+</button>
-        <ul>{this.state.notes.map((note) =>
-                       <li key={note.id}>{note.task}</li>
-                      )}</ul>
-        </div>);
-  },
+        <Notes notes={this.state.notes} onEdit={this.editNote} />
+        </div>
+    );
+  }
   
-  addNote: function() {
-    
+  addNote = () => {    
     this.setState({
       notes: this.state.notes.concat([{
         id: uuid.v4(),
         task: 'New task'
       }])
     });
-  }
-});
-
-export default App;
+  };
+  editNote = (id, task) => {
+    const notes = this.state.notes.map((note) => {
+      if (note.id === id && task) {
+        note.task = task;
+      }
+      return note;
+    });
+    this.setState({notes});
+  };
+}
